708945c65f687f235bbbbc1f3470c9d42d89b25a
Ridzwan <4947140+omgsian@users.noreply.github.com>
2024-09-30 01:04:42 +0800

    added the tables, schemas, and dags

---
+++ dags/d_account_generator.py
@@ -0,0 +1,80 @@
+
+from airflow import DAG
+import pandas as pd
+from datetime import datetime, timedelta
+import random
+from airflow.operators.empty import EmptyOperator
+from airflow.operators.python import PythonOperator
+
+start_date = datetime(2024, 09, 30)
+default_args = { 
+    'owner': 'mrh', 
+    'depends_on_past': False, 
+    'backfill': False,
+    # 'start_date': start_date
+}
+
+num_rows = 50
+output_file = './account_dim_large_data.csv'
+
+account_ids = []
+account_types = []
+statuses = []
+customer_ids  = []
+balances = []
+opening_dates = []
+
+def generate_random_data(row_num):
+    account_id = f'A{row_num:05d}'
+    account_types = random.choice(['Savings', 'Checking'])
+    status = random.choice(['Active', 'Inactive']) 
+    customer_id = f'C{random.randint(1,1000)}'         
+    balance = round(random.uniform(100.00, 10000.00))       
+
+    now = datetime.now()
+    random_date = now - timedelta(days=random.randint(1,365))
+    opening_date_millis = int(random_date.timestamp() * 1000)
+
+    return account_id, account_types, status, customer_id, balance, opening_date_millis
+
+def generate_account_dim_data():
+    row_num = 1
+    while row_num <= num_rows: 
+        account_id, account_types, status, customer_id, balance, opening_date_millis = generate_random_data(row_num)
+        account_ids.append(account_id)
+        account_types.append(account_types)
+        statuses.append(status)
+        customer_ids.append(customer_id)
+        balances.append(balance)
+        opening_dates.append(opening_date_millis)   
+        row_num += 1
+
+        df = pd.DataFrame({
+            'account_id': account_ids,
+            'account_type': account_types,
+            'status': statuses,
+            'customer_id': customer_ids,
+            'balance': balances,
+            'opening_date': opening_dates,
+        })
+
+        df.to_csv(output_file, index=False)
+
+        print(f'{num_rows} rows of data generated and saved to {output_file}')
+
+with DAG('d_account_generator', default_args=default_args,description='Generate large account dimensions data in a CSV file', schedule_interval=timedelta(days=1), start_date=start_date, tags=['schema']) as dag:
+    
+    start = EmptyOperator(
+        task_id='start_task'
+    )
+
+    generate_account_dim_data = PythonOperator(
+        task_id='generate_account_dim_data',
+        python_callable=generate_account_dim_data
+    )
+
+    end = EmptyOperator(
+        task_id='end_task'
+    )
+
+    start >> generate_account_dim_data >> end
---
+++ dags/d_branch_generator.py
@@ -0,0 +1,111 @@
+
+from airflow import DAG
+import pandas as pd
+from datetime import datetime, timedelta
+import random
+from airflow.operators.empty import EmptyOperator
+from airflow.operators.python import PythonOperator
+
+start_date = datetime(2024, 09, 15)
+default_args = { 
+    'owner': 'mrh', 
+    'depends_on_past': False, 
+    'backfill': False,
+    # 'start_date': start_date
+}
+
+num_rows = 50
+output_file = './branch_dim_large_data.csv'
+
+cities = [
+    "New York, New York",
+    "Los Angeles, California",
+    "Chicago, Illinois",
+    "Houston, Texas",
+    "Phoenix, Arizona",
+    "Philadelphia, Pennsylvania"
+    ]
+
+regions = [
+    "California",
+    "Texas",
+    "Florida",
+    "New York",
+    "Pennsylvania",
+    "Illinois",
+]
+
+postcodes = [
+    "90210",  # Beverly Hills, California
+    "10001",  # New York, New York
+    "77002",  # Houston, Texas
+    "60611",  # Chicago, Illinois
+    "19103",  # Philadelphia, Pennsylvania
+    "85004",  # Phoenix, Arizona
+]
+
+def generate_random_data(row_num):
+    branch_id = f'A{row_num:05d}'
+    branch_name = f'Branch {row_num}'
+    branch_address = f'{random.randomint(1,999)} {random.choice(["Main St", "Park Ave", "Oak St", "Maple Ave", "Elm St"])}'
+    city = random.choice(cities)
+    region = random.choice(regions)
+    postcode = random.choice(postcodes)
+
+    now = datetime.now()
+    random_date = now - timedelta(days=random.randint(1,365))
+    opening_date_millis = int(random_date.timestamp() * 1000)
+
+    return branch_id, branch_name, branch_address, city, region, postcode, opening_date_millis
+
+branch_ids = []
+branch_names = []
+branch_addresses = []
+cities = []
+regions = []
+postcodes = []
+opening_dates = []
+
+def generate_branch_dim_data():
+    row_num = 1
+    while row_num <= num_rows: 
+        data = generate_random_data(row_num)
+        branch_ids.append(data[0])
+        branch_names.append(data[1])
+        branch_addresses.append(data[2])
+        cities.append(data[3])
+        regions.append(data[4])
+        postcodes.append(data[5])
+        opening_dates.append(data[6])
+        row_num += 1
+
+        df = pd.DataFrame({
+            'branch_id': branch_ids,
+            'branch_names': branch_names,
+            'branch_addresses': branch_addresses,
+            'cities': cities,
+            'regions': regions,
+            'postcodes': postcodes,
+            'opening_dates': opening_dates,
+        })
+
+        df.to_csv(output_file, index=False)
+
+        print(f'{num_rows} rows of data generated and saved to {output_file}')
+
+with DAG('d_branch_generator', default_args=default_args,description='Generate large branch dimensions data in a CSV file', schedule_interval=timedelta(days=1), start_date=start_date, tags=['schema']) as dag:
+    
+    start = EmptyOperator(
+        task_id='start_task'
+    )
+
+    generate_branch_dim_data = PythonOperator(
+        task_id='generate_account_dim_data',
+        python_callable=generate_branch_dim_data
+    )
+
+    end = EmptyOperator(
+        task_id='end_task'
+    )
+
+    start >> generate_branch_dim_data >> end
